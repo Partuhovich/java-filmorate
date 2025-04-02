d60cc9b5f9513f0bb35387648199afab6c12091d
Pavel Art <partuhovich@gmail.com>
2025-04-02 13:59:01 +0300

    Sprint 10 solution

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,50 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private Map<Long, Film> films = new HashMap<>();
+    private Long idCounter = 1L;
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        film.validate();
+        log.info("Получен запрос на добавление фильма {}", film.getName());
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        log.info("Фильм {} был добавлен", film.getName());
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        film.validate();
+        log.info("Получен запрос на обновление фильма {}", film.getName());
+        Long id = film.getId();
+        if (!films.containsKey(id)) {
+            String errorMessage = String.format("Фильм с id %d не найден", id);
+            log.warn(errorMessage);
+            throw new RuntimeException(errorMessage);
+        }
+        films.replace(id, film);
+        log.info("Фильм {} был обновлен", film.getName());
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> getAll() {
+        log.info("Получен запрос на получение всех фильмов");
+        return new ArrayList<>(films.values());
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    private Long id;
+    private LocalDate releaseDate;
+
+    @NotBlank(message = "Название не может быть пустым или null")
+    private String name;
+
+    @Size(max = 200, message = "Описание не может превышать 200 символов")
+    private String description;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
+    private int duration;
+
+    public void validate() {
+        if (releaseDate.isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
 }
